6a5b5ec7f393293f093307d89e5e3ebd67ab8e02
insung1939 <insungcho1939@gmail.com>
2025-01-15 10:20:20 +0900

    Add Search Page

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from bs4 import BeautifulSoup
+import requests
 app = Flask("JObScrapper")
 
 @app.route("/") # decorator
@@ -6,8 +8,33 @@ def home():
     return render_template("home.html")
 
 @app.route('/search')
-def hello():
-    return render_template("search.html")
+def search():
+    keyword = request.args.get('keyword')
+
+    url = "https://weworkremotely.com/categories/remote-full-stack-programming-jobs#job-listings"
+
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    jobs = soup.find('section', class_='jobs').find_all('li')[1:-1]
+
+    job_db = []
+
+    for job in jobs:
+        title = job.find('span', class_='title').text
+        company, position, *region = job.find_all('span', class_='company')
+        url = job.find('div', class_="tooltip--flag-logo").next_sibling["href"]
+        job_data = {
+            'title': title,
+            'company': company.text,
+            'position': position.text,
+            'region': "No Region" if not len(region) else region[0].text,
+            "url": f"https://weworkremotelly.com{url}"
+        }
+        if keyword in job_data['title']:
+            job_db.append(job_data)
+    return render_template("search.html", keyword=keyword, jobs = job_db)
 
 if __name__ == "__main__":
     app.run(port=5001)  # 5001 또는 다른 사용 가능한 포트를 지정
